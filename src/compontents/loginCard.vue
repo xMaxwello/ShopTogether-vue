591a0a0bf32ef938a10d8740edd3e9db1a7c48b9
xMaxwello <lukas.p9199@gmail.com>
2024-07-15 14:56:39 +0200

    created Views and components

---
+++ src/compontents/loginCard.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isLoginView = ref(true);
+
+const toggleView = () => {
+  isLoginView.value = !isLoginView.value;
+};
+</script>
+
+<template>
+  <div class="flex flex-col w-[321px] h-auto py-4 bg-loginCard rounded-2xl items-center px-8 mt-24">
+    <span class="text-black text-[24px]">{{ isLoginView ? 'Anmeldung' : 'Registrieren' }}</span>
+
+    <ion-input
+        v-if="!isLoginView"
+        label="Vorname"
+        label-placement="floating"
+        type="text"
+        class="text-gray-500 mb-2"
+    />
+    <ion-input
+        v-if="!isLoginView"
+        label="Nachname"
+        label-placement="floating"
+        type="text"
+        class="text-gray-500 mb-2"
+    />
+
+    <ion-input
+        label="E-Mail"
+        label-placement="floating"
+        type="text"
+        class="text-gray-500 mb-2"
+    />
+
+    <ion-input
+        label="Passwort"
+        label-placement="floating"
+        type="password"
+        class="text-gray-500 mb-2"
+    />
+
+    <ion-input
+        v-if="!isLoginView"
+        label="Passwort wiederholen"
+        label-placement="floating"
+        type="password"
+        class="text-gray-500 mb-2"
+    />
+
+    <div class="flex flex-col">
+      <button v-if="isLoginView" class="bg-confirmButton text-white rounded-[24px] w-[161px] h-[35px] mb-2">
+        Anmelden
+      </button>
+      <button v-else class="bg-confirmButton text-white rounded-[24px] w-[161px] h-[35px] mb-2">
+        Registrieren
+      </button>
+      <button @click="toggleView" class="bg-white text-confirmButton rounded-[24px] w-[161px] h-[35px]">
+        {{ isLoginView ? 'Zur Registrierung' : 'Zur Anmeldung' }}
+      </button>
+    </div>
+  </div>
+</template>
+
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { IonContent, IonPage } from '@ionic/vue';
+import LoginCard from "@/compontents/loginCard.vue";
 </script>
 
 
@@ -11,17 +12,7 @@ import { IonContent, IonPage } from '@ionic/vue';
       <div class="flex w-full h-full flex-col items-center pt-24 bg-white">
         <span class="text-[40px] text-black">ShopTogether</span>
 
-        <div class="flex flex-col w-[321px] h-auto py-4 bg-loginCard rounded-2xl items-center px-8 mt-24">
-          <span class="text-black text-[24px]">Anmeldung</span>
-          <ion-input label="E-Mail" label-placement="floating" type="text" class="text-gray-500 mb-2"/>
-          <ion-input label="Passwort" label-placement="floating" type="password" class="text-gray-500 mb-2"/>
-
-          <div class="flex flex-col">
-            <button class="bg-confirmButton text-white rounded-[24px] w-[161px] h-[35px] mb-2">Anmelden</button>
-            <button class="bg-white text-confirmButton rounded-[24px] w-[161px] h-[35px]">Registrieren</button>
-          </div>
-
-        </div>
+        <LoginCard/>
 
       </div>
     </ion-content>
